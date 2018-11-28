9fbae8d63f7fc784b6639fa8e482a4f052a05692
Omega100 <d.fernandez.chaves@rug.nl>
2018-11-28 11:06:49 +0100

    lab2

---
+++ Lab2/Assigment1/Assigment1.m
@@ -0,0 +1,19 @@
+%Assigment1
+
+%Question 1-13
+Measurements = [4,6,8,7,4;5,3,7,4,6;6,9,3,8,5];
+
+mu = mean(Measurements')
+sigma = cov(Measurements',1)
+
+%Question 14
+base = [5 5 6];
+x = mvnpdf(base,mu,sigma);
+
+%Question 15
+base = [3 5 7];
+x = mvnpdf(base,mu,sigma);
+
+%Question 16
+base = [4 6.5 1];
+x = mvnpdf(base,mu,sigma);
