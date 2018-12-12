3b1934a2f02d96118fc902d5c90fea0ab216273b
Omega100 <d.fernandez.chaves@rug.nl>
2018-12-12 11:19:14 +0100

    Lab3 Ended

--- Lab3/Assignment1/Assignment1.m
+++ Lab3/Assignment1/Assignment1.m
@@ -3,8 +3,11 @@ mu = [5 7];
 Sigma = 4;
 
 %Question1
-fa=1-normcdf(10,mu(1),Sigma);
 h=1-normcdf(10,mu(2),Sigma);
+
+%Question2
+fa=1-normcdf(10,mu(1),Sigma);
+
 
 %Question3
 d1=abs(5-7)/sqrt(Sigma);
@@ -16,11 +19,31 @@ u2=3*sqrt(4)+5
 
 %Question7
 load('lab3_1','outcomes' );
-h = sum(outcomes(:,1) + outcomes(:,2) == 2)/size(outcomes,1);
+h = sum(outcomes(:,1) + outcomes(:,2) == 2)/sum(outcomes(:,1)==1);
 %Question8
-fa=sum(and(outcomes(:,1)==0, outcomes(:,2) == 1))/size(outcomes,1);
+fa=sum(and(outcomes(:,1)==0, outcomes(:,2) == 1))/sum(outcomes(:,1)==0);
+
 %Question9
+base = -5:.1:5;
 
+figure
+hold on
+axis([0 1 0 1])
+title('ROC');
+xlabel('1-Specificity');
+ylabel('Sensitivity');
+plot(fa,h,'or')
 
+TruePositive=1-normcdf(base,0,1);
+
+for i=0:5
+    FalsePositive=1-normcdf(base,i,1);
+    plot(TruePositive,FalsePositive,'-')
+end
+
+FalsePositive=1-normcdf(base,2.125,1);
+plot(TruePositive,FalsePositive,'-')
+
+legend('Point Question 7 and 8','d=0','d=1','d=2','d=3','d=4','d=5','d=2.125')
 
 
--- Lab3/Assignment3/Assignment3.m
+++ Lab3/Assignment3/Assignment3.m
@@ -1,1 +1,57 @@
 %Assignment3
+load('lab3_3_cat1.mat')
+load('lab3_3_cat2.mat')
+load('lab3_3_cat3.mat')
+
+u=[0.5;1;0];
+v=[0.31;1.51;-0.5];
+w=[-1.7;-1.7;-1.7];
+prior = 1/3;
+
+h=1;
+x=v;
+
+%Class1
+probability_density=0;
+for i=1:10
+    probability_density=probability_density+exp(-((x(1)-x_w1(i,1))^2+(x(2)-x_w1(i,2))^2+(x(3)-x_w1(i,3))^2)/(2*h)^2);
+end
+
+%Normalization
+probability_density = probability_density/(h*sqrt(2*pi))^3;
+probability_density1 = probability_density/size(x_w1,1);
+
+
+%Class2
+probability_density=0;
+for i=1:10
+    probability_density=probability_density+exp(-((x(1)-x_w2(i,1))^2+(x(2)-x_w2(i,2))^2+(x(3)-x_w2(i,3))^2)/(2*h)^2);
+end
+
+%Normalization
+probability_density = probability_density/(h*sqrt(2*pi))^3;
+probability_density2 = probability_density/size(x_w2,1);
+
+%Class3
+probability_density=0;
+for i=1:10
+    probability_density=probability_density+exp(-((x(1)-x_w3(i,1))^2+(x(2)-x_w3(i,2))^2+(x(3)-x_w3(i,3))^2)/(2*h)^2);
+end
+
+%Normalization
+probability_density = probability_density/(h*sqrt(2*pi))^3;
+probability_density3 = probability_density/size(x_w3,1);
+
+
+posterior1 = (probability_density1*prior);
+posterior2 = (probability_density2*prior);
+posterior3 = (probability_density3*prior);
+
+posterior1_norm = posterior1/(posterior1+posterior2+posterior3)
+posterior2_norm = posterior2/(posterior1+posterior2+posterior3)
+posterior3_norm = posterior3/(posterior1+posterior2+posterior3)
+
+datas=[x_w1; x_w2; x_w3];
+classes=[1+zeros(size(x_w1,1),1);2+zeros(size(x_w1,1),1);3+zeros(size(x_w1,1),1)];
+KNN(x,5,datas,classes)
+
