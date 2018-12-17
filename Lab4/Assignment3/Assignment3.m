442c8aea6aab69e7bb3492561c677ab68784154c
S3836452 <S3836452@XAP022.workspace.rug.nl>
2018-12-17 19:42:38 +0100

    lines done

--- Lab4/Assignment3/Assignment3.m
+++ Lab4/Assignment3/Assignment3.m
@@ -4,6 +4,14 @@ BW = edge(I,'canny');
 P = houghpeaks(H,15,'threshold', ceil(0.3*max(H(:))));
 x = T(P(:,2));
 y = R(P(:,1));
+figure;
 plot(x,y,'s','color','white');
 set(gca,'Color','k');
 title('15 Strongest points on the Hough space');
+
+figure;
+imshow(I);
+for i = 1:size(P,1)
+    myhoughline(I,y(i),x(i));
+end
+title('15 Strongest lines on the image')
