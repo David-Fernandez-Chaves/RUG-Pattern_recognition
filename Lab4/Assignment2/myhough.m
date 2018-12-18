97d1fdf541e8eaf8f21993d600443144b4b53aa8
S3836452 <S3836452@XAP023.workspace.rug.nl>
2018-12-18 15:08:28 +0100

    rectified myhough

--- Lab4/Assignment2/myhough.m
+++ Lab4/Assignment2/myhough.m
@@ -1,23 +1,24 @@
 function H = myhough(edge_map)
 figure;
 imshow(edge_map);
-[row, column] = find(edge_map); %foreground in an edge map corresponds to edges
-calculations = zeros(size(row,1),180);
+[rows, columns] = find(edge_map); %foreground in an edge map corresponds to edges
+calculations = zeros(size(rows,1),180);
 for theta = -89:90
-    calculations(:,theta+90) = round((column.*cosd(theta)+row.*sind(theta)));
+    calculations(:,theta+90) = round((columns.*cosd(theta)+rows.*sind(theta)));
 end
 pmax = max(calculations,[],'all'); %max ro
-H = zeros(pmax*2,180);
+H = zeros(pmax*2,180); %accumulator array
 for degree = 1:180
-    H(calculations(:,degree)+pmax,degree) = H(calculations(:,degree)+pmax,degree) +1;
+    for values = 1:size(rows,1)
+        H(calculations(values,degree)+pmax,degree) = H(calculations(values,degree)+pmax,degree) +1;
+    end
 end
 % [x y]=size(edge_map);
 % DiagonalImage=sqrt(x^2+y^2);
-% 
 % H = zeros(2*round(DiagonalImage)+1,180+1);
 % 
-% for colum=1:x
-%     for row=1:y
+% for colum= columns
+%     for row=rows
 %        for theta=-89:1:91
 %            r=round((colum*cosd(theta)+row*sind(theta)+DiagonalImage))+1;
 %            if r < size(H,1)
