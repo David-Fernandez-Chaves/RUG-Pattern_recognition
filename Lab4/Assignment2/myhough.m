b9f8de717c772df10adaabc8487a9226b1f8440a
S3836452 <S3836452@XAP023.workspace.rug.nl>
2018-12-18 12:00:10 +0100

    comparacion

--- Lab4/Assignment2/myhough.m
+++ Lab4/Assignment2/myhough.m
@@ -7,7 +7,7 @@ H = zeros(2*round(DiagonalImage)+1,180+1);
 
 for colum=1:x
     for row=1:y
-       for theta=-89:1:90
+       for theta=-89:1:91
            r=round((colum*cosd(theta)+row*sind(theta)+DiagonalImage))+1;
            if r < size(H,1)
                H(r,round(theta+90))=H(r,round(theta+90))+1;
