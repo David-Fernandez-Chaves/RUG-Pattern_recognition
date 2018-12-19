fef3db22a6119124760f088789608858d524f2ed
RoboticsNose <omega100@gmail.com>
2018-12-19 01:41:21 +0100

    Lab 4, Assignment2 Updated

--- Lab4/Assignment2/myhough.m
+++ Lab4/Assignment2/myhough.m
@@ -1,33 +1,32 @@
 function H = myhough(edge_map)
-figure;
-imshow(edge_map);
-[rows, columns] = find(edge_map); %foreground in an edge map corresponds to edges
-calculations = zeros(size(rows,1),180); %ro's
-for theta = -89:90
-    calculations(:,theta+90) = round((columns.*cosd(theta)+rows.*sind(theta))); %calculate all ro's from a point
-end
-pmax = max(calculations,[],'all') %max ro 
-H = zeros(pmax*2,180); %accumulator array
-for degree = 1:180
-    for values = 1:size(rows,1)
-        H(calculations(values,degree)+pmax,degree) = H(calculations(values,degree)+pmax,degree) +1; % H(ro,zeta) = H(ro,zeta) +1
-    end
-end
-% [x y]=size(edge_map);
-% DiagonalImage=sqrt(x^2+y^2);
-% H = zeros(2*round(DiagonalImage)+1,180+1);
-% 
-% for colum= columns
-%     for row=rows
-%        for theta=-89:1:91
-%            r=round((colum*cosd(theta)+row*sind(theta)+DiagonalImage))+1;
-%            if r < size(H,1)
-%                H(r,round(theta+90))=H(r,round(theta+90))+1;
-%            end
-%        end
-%     end    
+% figure;
+% imshow(edge_map);
+    [rows, columns] = find(edge_map); %foreground in an edge map corresponds to edges
+% calculations = zeros(size(rows,1),180); %ro's
+% for theta = -89:90
+%     calculations(:,theta+90) = round((columns.*cosd(theta)+rows.*sind(theta))); %calculate all ro's from a point
+% end
+% pmax = max(calculations,[],'all') %max ro 
+% H = zeros(pmax*2,180); %accumulator array
+% for degree = 1:180
+%     for values = 1:size(rows,1)
+%         H(calculations(values,degree)+pmax,degree) = H(calculations(values,degree)+pmax,degree) +1; % H(ro,zeta) = H(ro,zeta) +1
+%     end
 % end
 
+
+
+    [x y]=size(edge_map);
+    DiagonalImage=sqrt(x^2+y^2);
+    H = zeros(2*round(DiagonalImage),180);
+    for p=1:size(rows,1)
+           for theta=-90:1:89
+               r=round((columns(p)*cosd(theta)+rows(p)*sind(theta)+DiagonalImage))+1;
+               if r < size(H,1)
+                   H(r,round(theta+91))=H(r,round(theta+91))+1;
+               end
+           end
+    end
 
 end
 
