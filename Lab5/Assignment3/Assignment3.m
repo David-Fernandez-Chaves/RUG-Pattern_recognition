d277f4acf0253ca642aa62314fa1706296c72a43
s3836452 <s3836452@XAP027.workspace.rug.nl>
2019-01-01 14:10:51 +0100

    done right

---
+++ Lab5/Assignment3/Assignment3.m
@@ -0,0 +1,37 @@
+%Assignment2
+close all
+%Setting and Load Datas
+nu=0.01;
+nWA = 2;
+nWB = 1;
+lambdas = [0.5 0.5];
+A = importdata('data_lvq_A(1).mat');
+B = importdata('data_lvq_B(1).mat');
+%% Plot the init data
+figure;hold on;
+scatter(A(:,1),A(:,2),'r','filled');
+scatter(B(:,1),B(:,2),'b','filled');
+xlabel('Feature 1');
+ylabel('Feature 2');
+%% Tag by class
+[p,n] = size(A);
+data = zeros(p*2,n+1);
+data(:,1:2) = [A;B];
+data(1:p,3) = 1; %points of class A
+data(p+1:end,3) = 2; %points of class B
+%rng(9); %fix randperm
+data = data(randperm(p*2),:); %shuffle the data
+%% Training
+[lambdasVector,w,trainingErrorVector] = RLVQ_learning(data,nu,nWA,nWB,lambdas);
+%% Plot final prototypes
+scatter(w(:,1),w(:,2),'g','filled');
+%% Training Error and lambdas progress
+figure; hold on;
+subplot(1,2,1);
+plot(trainingErrorVector);
+xlabel('Epochs');
+ylabel('Training Error(%)');
+subplot(1,2,2);
+plot(lambdasVector(:,:));
+xlabel('Epochs');
+ylabel('lambdas');
---
+++ Lab5/Assignment3/RLVQ_learning.m
@@ -0,0 +1,51 @@
+function [lambdasVector,w,TrainingErrorVector] = RLVQ_learning(datas,nu,nWA,nWB,lambdas)
+%LVQ1 learning for two class
+    %data with class tags
+    %learning percentage
+    %number of class A prototypes
+    %number of class B prototypes
+    OldTrainingError=1;
+    TrainingError=0;
+    lambdasVector = lambdas;
+    TrainingErrorVector = [];
+    %% Prototypes initialization
+    w = zeros(nWA+nWB,3);
+    classA = datas(datas(:,3) == 1,:);
+    w(1:nWA,:) = classA(randperm(size(classA,1),nWA),:); %prototypes of class A 
+    classB = datas(datas(:,3) == 2,:);
+    w(nWA+1:end,:) = classB(randperm(size(classB,1),nWB),:); %prototypes of class B 
+    %% Training
+    while abs(OldTrainingError-TrainingError)>0.000000001
+        for n=1:size(datas,1)
+            dif=(w(:,1:2)-datas(n,1:2)); %Get the difference
+            distance = sum(lambdas.*(dif.^2),2); %squared euclidean norm
+            [~,i] = min(distance); %Find the minimum distance
+            if w(i,3) ~= datas(n,3) %Check if the prototype and the data are of equal classes
+                w(i,1:2)=w(i,1:2)+nu*(dif(i,:)); %Different classes
+                lambdas = lambdas + nu*(dif(i,:)); %update lambdas
+            else
+                w(i,1:2)=w(i,1:2)-nu*(dif(i,:)); %Same class
+                lambdas = lambdas - nu*(dif(i,:)); %update lambdas
+            end
+            lambdas = lambdas/sum(lambdas); %normalize lambdas
+        end
+
+        % Geting Training Error
+        missclassified=0;
+        for n=1:size(datas,1)
+            dif=(w(:,1:2)-datas(n,1:2)); %Get the difference
+            distance = sum(lambdas.*(dif.^2),2); %squared euclidean norm
+            [~,i] = min(distance); %Find the minimum distance
+            if w(i,3) ~= datas(n,3) %Check if the prototype and the data are of equal classes
+                missclassified=missclassified+1;%Different classes
+            end
+
+        end
+        OldTrainingError=TrainingError;
+        TrainingError=missclassified/size(datas,1);
+        TrainingErrorVector = [TrainingErrorVector; TrainingError]; %training error for epoch
+        lambdasVector = [lambdasVector; lambdas]; %lambdas for epoch
+    end
+
+end
+
