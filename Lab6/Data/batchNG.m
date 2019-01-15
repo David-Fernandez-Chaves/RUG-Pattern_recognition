713985d2465d00eed91fd210d4c06356765978b3
S3836452 <S3836452@XAP021.workspace.rug.nl>
2019-01-16 00:49:33 +0100

    bng done

--- Lab6/Data/batchNG.m
+++ Lab6/Data/batchNG.m
@@ -1,5 +1,4 @@
 function [prototypes] = batchNG(Data, n, epochs, xdim, ydim)
-
 % Batch Neural Gas
 %   Data contains data,
 %   n is the number of clusters,
@@ -11,14 +10,16 @@ if (nargin<4)
   xdim=1; ydim=2;   % default plot values
 end
 
-[dlen,dim] = size(Data);
-
-%prototypes =  % small initial values
-% % or
+%prototypes init
 sbrace = @(x,y)(x{y});
 fromfile = @(x)(sbrace(struct2cell(load(x)),1));
 prototypes=fromfile('clusterCentroids.mat');
-
+figure;hold on;
+plot(Data(:,xdim),Data(:,ydim),'bo','markersize',3)
+plot(prototypes(:,xdim),prototypes(:,ydim),'r.','markersize',10,'linewidth',3)
+voronoi(prototypes(:,xdim),prototypes(:,ydim))
+title('Initial prototypes - Broken algorithm')
+figure;
 lambda0 = n/2; %initial neighborhood value
 % lambda
 lambda = lambda0 * (0.01/lambda0).^([0:(epochs-1)]/epochs);
@@ -27,26 +28,20 @@ lambda = lambda0 * (0.01/lambda0).^([0:(epochs-1)]/epochs);
 ss=1;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Action
-
 for i=1:epochs
-  D_prototypes_av = zeros(n,dlen);       % the same holds for the quotients
-
- [~,D_prototypes] = sort(pdist2(prototypes,Data));
- lambda(i)
-  prototypes = exp(-(D_prototypes/lambda(i)))*Data./sum(exp(-(D_prototypes/lambda(i))),2);
-  
-  % track
-  if ismember(i,[20,100,200,500])   %plot each epoch
-    %fprintf(1,'%d / %d \r',i,epochs);
-    subplot(2,2,ss);
-    hold on
-    plot(Data(:,xdim),Data(:,ydim),'bo','markersize',3)
-    plot(prototypes(:,xdim),prototypes(:,ydim),'r.','markersize',10,'linewidth',3)
-    % write code to plot decision boundaries
-    voronoi(prototypes(:,xdim),prototypes(:,ydim))
-    %pause
-    %or
-    ss=ss+1;
-  end
+    %Rank prototype by distance to each points
+    [~,D_prototypes] = sort(pdist2(prototypes,Data));
+    %Update prototypes
+    prototypes = exp(-(D_prototypes/lambda(i)))*Data./sum(exp(-(D_prototypes/lambda(i))),2);
+    %Plot
+    if ismember(i,[20,100,200,500])   
+        subplot(2,2,ss);
+        hold on
+        plot(Data(:,xdim),Data(:,ydim),'bo','markersize',3)
+        plot(prototypes(:,xdim),prototypes(:,ydim),'r.','markersize',10,'linewidth',3)
+        % write code to plot decision boundaries
+        voronoi(prototypes(:,xdim),prototypes(:,ydim))
+        ss=ss+1;
+    end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
