a01beb05ad3c757b1dfa1f93c6d90c126f436d74
S3836452 <S3836452@XAP030.workspace.rug.nl>
2019-01-19 20:57:25 +0100

    added distance matrix

--- Lab7/Assignment3/Assignment3.m
+++ Lab7/Assignment3/Assignment3.m
@@ -14,3 +14,8 @@ m2=mean(c2,2);
 % ATTENTION all only works on 2018b edition
 Je = sum((c1-m1).^2,'all'); %since we don't have to submit the code 
 Je = Je + sum((c2-m2).^2,'all'); %and is only two clusters
+
+%revise, calculate all distance
+x = [x1 x2 x3 x4 x5];
+z = pdist(x');
+z = squareform(z);
